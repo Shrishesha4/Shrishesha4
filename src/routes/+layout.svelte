0d72dab1ee2464e3708bd832d67063e53b25585b
Shrishesha <shrisheshanarmatesshvara@gmail.com>
2025-01-24 19:26:26 +0530

    v1

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import '../app.css';
+	// import 'flowbite/dist/flowbite.css';
+	let { children } = $props();
+
+// @ts-nocheck
+    import { Navbar, NavBrand, NavLi, NavUl, NavHamburger, Button } from 'flowbite-svelte';
+</script>
+  
+<Navbar>
+    <NavBrand href="/" class="w-auto md:w-[67vw]">
+        <img src="/favicon.png" class="me-3 h-6 sm:h-9" alt="Flowbite Logo" />
+        <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Shrishesha</span>
+    </NavBrand>
+    <div class="flex md:order-2">
+        <Button size="sm">Get started</Button>
+        <NavHamburger />
+    </div>
+    <NavUl class="order-1">
+        <NavLi href="/" activeClass="active">Home</NavLi>
+        <NavLi href="/aboutme">About</NavLi>
+        <NavLi href="/projects">Projects</NavLi>
+        <NavLi href="/contact">Contact</NavLi>
+    </NavUl>
+</Navbar>
+
+{@render children()}
