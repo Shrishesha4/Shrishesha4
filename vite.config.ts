ac7419d39a186c8ae81ed5184e935cd3dc77b778
Shrishesha <shrisheshanarmatesshvara@gmail.com>
2025-01-30 12:09:17 +0530

    fixed vite.config.ts for v7

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
         rollupOptions: {
             output: {
                 manualChunks: {
-                    'vendor': ['axios', 'firebase/app', 'firebase/auth'],
+                    'firebase': ['firebase/app', 'firebase/auth', 'firebase/firestore'],
                     'ui': ['@melt-ui/svelte']
                 }
             }
