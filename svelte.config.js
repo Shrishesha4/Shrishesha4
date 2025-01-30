4bb5a1af3eda4ab5b5f120b3b60b582fa3034f67
Shrishesha <shrisheshanarmatesshvara@gmail.com>
2025-01-30 13:38:07 +0530

    fixed maybe?

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,19 @@
 import { preprocessMeltUI, sequence } from '@melt-ui/pp';
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     kit: {
-        adapter: adapter()
+        adapter: adapter(),
+        csrf: {
+            checkOrigin: false,
+        }
     },
-    preprocess: vitePreprocess()
+    preprocess: sequence([
+        vitePreprocess(),
+        preprocessMeltUI()
+    ])
 };
 
 export default config;
