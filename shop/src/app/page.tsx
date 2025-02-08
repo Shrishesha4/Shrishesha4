990e86e9a5ce847fbfc4d719c4de459922e066bf
Shrishesha <shrisheshanarmatesshvara@gmail.com>
2025-02-08 11:15:55 +0530

    added shop changes

--- shop/src/app/page.tsx
+++ shop/src/app/page.tsx
@@ -41,7 +41,7 @@ export default function Home() {
     }, 2000);
 
     return () => clearInterval(timer);
-  }, [phrases]); // Added phrases to dependency array
+  }, [phrases]);
 
   return (
     <div className="min-h-screen flex flex-col items-center justify-between p-8 text-center relative overflow-hidden">
