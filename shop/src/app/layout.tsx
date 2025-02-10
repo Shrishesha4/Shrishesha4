2c6c58d0cd2923f505e7dc6e1d0c0d178df2b7cc
Shrishesha <shrisheshanarmatesshvara@gmail.com>
2025-02-11 00:33:48 +0530

    added analytics to shop

--- shop/src/app/layout.tsx
+++ shop/src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
+
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,6 +31,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
